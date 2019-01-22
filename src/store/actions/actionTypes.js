e7c6a97aff0a66c8b82333a16097e0fbd909587a
Aung Khant <theaungkhant@gmail.com>
2019-01-22 12:02:13 +0000

    Redux: Info Actions

---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,4 @@
+export const ADD_INFO_START = 'ADD_INFO_START';
+export const ADD_INFO_SUCCESS = 'ADD_INFO_SUCCESS';
+export const ADD_INFO_FAIL = 'ADD_INFO_FAIL';
+export const INFO_INIT = 'INFO_INIT';
